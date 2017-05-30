f99cb9dbbdd2f70c59a54eb8498f95837a78814c
Kiên Nguyễn <nguyenphuctrungkien@gmail.com>
2017-05-30 13:35:30 +0700

    Success

--- DemoRedux/src/components/Student.js
+++ DemoRedux/src/components/Student.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet,TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-import {fetchData} from '../actions'
+import { fetchData } from '../actions'
 
 class Student extends Component {
-   
+
     render() {
-      console.log(this.props);
+
         return (
             <View style={styles.container}>
                 <TouchableOpacity style={styles.button} onPress={this.props.fetchData}>
                     <Text>Click Me</Text>
                 </TouchableOpacity>
-               
+                <View style={styles.mainContent}>
+                    {
+                        this.props.data.isFetching && <Text>Loading</Text>
+                    }
+                    {
+                        this.props.data.data.length ? (
+                            this.props.data.data.map((person, i) => {
+                                return <View key={i} >
+                                    <Text>Name: {person.name} - Age: {person.edu}</Text>
+                                    
+                                </View>
+                            })
+                        ) : null
+                    }
+                </View>
+
             </View>
         );
     }
@@ -22,22 +37,23 @@ const styles = StyleSheet.create({
         flex: 1
     },
     button: {
-        height: 50 ,
+        height: 50,
         margin: 20,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'aqua',
-        borderRadius: 10 
-        
-    },
-    content: {
+        borderRadius: 10
 
-    }
+    },
+  mainContent: {
+    margin: 10,
+  }
 })
-const mapStateToProps=(state) => {
+const mapStateToProps = (state) => {
+    console.log('AAAA');
     console.log(state);
     return {
         data: state.fetchReducers
     }
 }
-export default connect(mapStateToProps,{fetchData})(Student)
+export default connect(mapStateToProps, { fetchData })(Student)
